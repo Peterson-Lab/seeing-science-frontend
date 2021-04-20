210f02e529e5562f39ba9fbcc4d83119e1045bbd
Matt Wilkinson <mattwilki17@gmail.com>
2021-04-19 23:59:08 -0400

    favicon and pagetitle

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Footer } from './Footer'
 import Navbar from './Navbar'
+import Head from 'next/head'
 
-const Layout: React.FC = ({ children }) => {
+interface LayoutProps {
+  pageTitle?: string
+}
+
+const Layout: React.FC<LayoutProps> = ({ children, pageTitle }) => {
+  const title = pageTitle ? pageTitle : 'Seeing Science'
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content={title} key="title" />
+      </Head>
       <Navbar />
       {children}
       <Footer />
